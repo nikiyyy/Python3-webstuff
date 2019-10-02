24f9cc587c29244715835ffa97a4623a0d7ea9e8
nikiyyy <44340403+nikiyyy@users.noreply.github.com>
2019-10-02 12:38:28 +0300

    Add files via upload

---
+++ piratebay.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+siteproxy="https://pirateproxy.ch"
+search_therm="shrek"
+def pages(br):
+    cur_br=0
+    while cur_br<=br:
+        print("in")
+        site=requests.get(siteproxy+"/search/"+search_therm+"/"+str(cur_br)+"/")
+        site_con=site.content
+        soup=bs(site_con,"html.parser")
+        for link in soup.findAll('a',{"class":"detLink"}):
+            title = link.get('title')
+            print(title)
+        cur_br+=1
+        print("new page")
+    
+pages(2)
